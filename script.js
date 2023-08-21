232db98c3d67ec15823d0cd77859d1fc6b9afd0c
Anirudh Sudhir <137381184+AnirudhSudhir@users.noreply.github.com>
2023-08-21 17:28:53 +0530

    Add equals function and prevent display of answer during single operations

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function operateNumbers(num1, num2, operator) {
+function calculateNumbers(num1, num2, operator) {
     if (operator === 'add') return num1 + num2;
     else if (operator === 'subtract') return num1 - num2;
     else if (operator === 'multiply') return num1 * num2;
@@ -6,19 +6,33 @@ function operateNumbers(num1, num2, operator) {
 }
 
 function readNumber(e) {
-    if (num1 === null) { num1 = e.target.getAttribute('data-key') }
+    if (num1 === null) num1 = Number(e.target.getAttribute('data-number'));
+    else num2 = Number(e.target.getAttribute('data-number'));
+}
+
+function computeOperator(e) {
+    if (num2 === null)
+        operator = e.target.getAttribute('data-operator');
     else {
-        num2 = e.target.getAttribute('data-key');
-        answer = operateNumbers(num1, num2, operator);
+        answer = calculateNumbers(num1, num2, operator);
         console.log(answer);
         num1 = answer;
+        num2 = null;
+        operator = e.target.getAttribute('data-operator');
     }
+}
+
+function operateNumbers() {
+    answer = calculateNumbers(num1, num2, operator);
+    console.log(answer);
+    num1 = null;
+    num2 = null;
 }
 
 const digitButtons = document.querySelectorAll('.digits button');
 digitButtons.forEach(digitButton => digitButton.addEventListener('click', readNumber));
 const operatorButtons = document.querySelectorAll('.operators button');
-operatorButtons.forEach(operatorButton => operatorButton.addEventListener('click', e => {
-    operator = e.target.getAttribute('data-key');
-}));
+operatorButtons.forEach(operatorButton => operatorButton.addEventListener('click', computeOperator));
+const calculateButton = document.querySelector('#calculate');
+calculateButton.addEventListener('click', operateNumbers)
 let num1 = null, num2 = null, operator = null, answer = null;
